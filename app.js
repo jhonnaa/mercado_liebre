81dde9308aa8fa371cbcf4c6e829bb99e51164bf
jhonnaa <jonnyemeni@gmail.com>
2021-11-23 11:56:45 -0300

    agregando proctfile

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express= require('express');
 const app = express()
 app.use(express.static('public'));
 
-app.listen(3030, ()=>{
-    console.log("servidor funcionando")
-})
+app.listen (process.env.PORT ||3000, ()=>{
+    console.log('Servidor funcionando bien');
+});
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
 })
